5f74b8572e819d124c0b02d24f353b5883f50233
Caroline Mimeault <carolinemimeault@gmail.com>
2016-02-22 22:39:38 -0500

    R file for funding

---
+++ quantifying_collaboration_funding.R
@@ -0,0 +1,7 @@
+require(igraph)
+edges <- read.csv("/home/caroline/Documents/projects/probable-barnacle/finfish_freshwater_funding_edges.csv")
+vertices <- read.csv("/home/caroline/Documents/projects/probable-barnacle/finfish_freshwater_funding_edges.csv")
+g <- graph_from_data_frame(edges,directed=FALSE,vertices=NULL)
+layout <- layout.kamada.kawai(g)
+plot(g, layout=layout, vertex.color= V(g)$Group_Color)
+         
